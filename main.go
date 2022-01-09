31996f247a3fac50c857c99af218bbff99883f6d
135e2 <135e2@135e2.tk>
2022-01-09 16:24:46 +0800

    refactor!: create all widgets manually
    - We don't need any *.ui file anymore, as they seem to be complex and stupid.
    - Also renamed appId
    
    Signed-off-by: 135e2 <135e2@135e2.tk>

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
+	"github.com/gotk3/gotk3/glib"
 	"io"
 	"log"
 	"net/url"
@@ -11,11 +11,10 @@ import (
 	"path/filepath"
 	"time"
 
-	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 )
 
-const appId = "com.experimental.annie-gtk"
+const appId = "com.github-135e2.annie-gtk"
 
 var (
 	DestFolder string
@@ -28,167 +27,182 @@ type outputBuffer struct {
 }
 
 func main() {
-	// Create a new application.
-	application, err := gtk.ApplicationNew(appId, glib.APPLICATION_FLAGS_NONE)
+	app, err := gtk.ApplicationNew(appId, glib.APPLICATION_FLAGS_NONE)
+
+	if err != nil {
+		log.Fatal("Could not create application.", err)
+	}
+
+	app.Connect("activate", func() {
+		onActivate(app)
+	})
+
+	app.Run(os.Args)
+}
+
+func onActivate(application *gtk.Application) {
+	// Create a new application window.
+	win := setupWindow("annie-gtk", application)
+	mainbox := setupBox(gtk.ORIENTATION_VERTICAL)
+	win.Add(mainbox)
+
+	// Setup menubar
+	menubar := setupMenuBar()
+	menuitem1 := setupMenuItem()
+	menubar.Append(menuitem1)
+	mainbox.PackStart(menubar, false, true, 0)
+
+	// Setup box1
+	box1 := setupBox(gtk.ORIENTATION_HORIZONTAL)
+	label1 := setupLabel("视频链接")
+	box1.PackStart(label1, false, true, 0)
+	entry1, err := gtk.EntryNew()
 	errorCheck(err)
+	box1.PackStart(entry1, true, true, 0)
+	mainbox.PackStart(box1, false, true, 0)
+
+	// Setup box2
+	box2 := setupBox(gtk.ORIENTATION_HORIZONTAL)
+	label2 := setupLabel("目标文件夹")
+	box2.PackStart(label2, false, true, 0)
+	filebutton, err := gtk.FileChooserButtonNew("选择文件", gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+	box2.PackStart(filebutton, false, true, 0)
+	startbutton, err := gtk.ButtonNewWithLabel("开始下载")
+	errorCheck(err)
+	startbutton.SetMarginStart(256)
+	startbutton.SetMarginEnd(256) // Set Margin to 256
+	box2.PackStart(startbutton, true, true, 0)
+	mainbox.PackStart(box2, false, true, 0)
+
+	// Setup box3
+	box3 := setupBox(gtk.ORIENTATION_HORIZONTAL)
+	label3 := setupLabel("下载进度")
+	box3.PackStart(label3, false, true, 0)
+	progbar, err := gtk.ProgressBarNew()
+	progbar.SetShowText(true)
+	box3.PackStart(progbar, true, true, 0)
+	mainbox.PackStart(box3, false, true, 0)
+
+	// Setup scrolled window
+	scrolledwindow, err := gtk.ScrolledWindowNew(nil, nil)
+	errorCheck(err)
+	textview := setupTextView()
+	scrolledwindow.Add(textview)
+	mainbox.PackStart(scrolledwindow, true, true, 0)
 
 	// Get exec path
 	ex, err := os.Executable()
 	errorCheck(err)
 	exPath := filepath.Dir(ex) + string(os.PathSeparator)
-	// Connect function to application activate event
-	application.Connect("activate", func() {
-		// Get the GtkBuilder UI definition in the glade file.
-		builder, err := gtk.BuilderNewFromFile(exPath + "annie-gtk.ui")
+
+	// Deal with signals
+	filebutton.Connect("current-folder-changed", func() {
+		DestFolder, err = filebutton.GetCurrentFolder()
 		errorCheck(err)
+	})
 
-		Window1obj, err := builder.GetObject("window1")
+	startbutton.Connect("clicked", func() {
+		URL, err = entry1.GetText()
 		errorCheck(err)
-
-		// Verify that the object is a pointer to a gtk.ApplicationWindow.
-		win, err := isWindow(Window1obj)
+		DestFolder, err = filebutton.GetCurrentFolder()
 		errorCheck(err)
-		win.SetTitle("annie-gtk")
+		if checkURL(URL) {
+			AddText(textview, "Download started")
+			progbar.SetFraction(0) // Reset ProgressBar
+			AddText(textview, fmt.Sprintf("annie-gtk is now downloading %s => %s", URL, DestFolder))
+			// TODO: Download progress
+			output := &outputBuffer{
+				reader:   nil,
+				textview: textview,
+			}
+			savedStdout := os.Stdout
+			r, w, _ := os.Pipe()
+			output.attachReader(r, textview)
+			os.Stdout = w
 
-		Entry1obj, err := builder.GetObject("entry1")
-		errorCheck(err)
-		entry1 := Entry1obj.(*gtk.Entry)
+			defaultDownloader, data, err := setupDownloader(nil, URL)
+			if err != nil {
+				AddText(textview, "annie-backend got error while setting up downloader: "+err.Error())
+			}
+			err, Site, Title, Type, Size, FileNameLength, stream := GetInfo(defaultDownloader, data)
+			if err != nil {
+				AddText(textview, "annie-backend got error: "+err.Error())
+			}
 
-		StartButtonobj, err := builder.GetObject("startbutton")
-		startbutton := StartButtonobj.(*gtk.Button)
-
-		FileButtonobj, err := builder.GetObject("filebutton")
-		errorCheck(err)
-		filebutton := FileButtonobj.(*gtk.FileChooserButton)
-		filebutton.SetCurrentFolder("./")
-
-		Textviewobj, err := builder.GetObject("textview")
-		textview := Textviewobj.(*gtk.TextView)
-
-		MenuItem1obj, err := builder.GetObject("menuitem1")
-		menuitem1 := MenuItem1obj.(*gtk.MenuItem)
-
-		ProgBarobj, err := builder.GetObject("progbar")
-		progbar := ProgBarobj.(*gtk.ProgressBar)
-
-		Aboutdialogobj, err := builder.GetObject("aboutdialog")
-		aboutdialog := Aboutdialogobj.(*gtk.AboutDialog)
-		aboutdialog.SetVersion(version)
-		aboutdialog.SetTitle("About Page")
-
-		// Deal with signals
-		filebutton.Connect("file-set", func() {
-			DestFolder, err = filebutton.GetCurrentFolder()
-			errorCheck(err)
-		})
-
-		startbutton.Connect("clicked", func() {
-			URL, err = entry1.GetText()
-			errorCheck(err)
-			DestFolder, err = filebutton.GetCurrentFolder()
-			errorCheck(err)
-			if checkURL(URL) {
-				AddText(textview, "Download started")
-				progbar.SetFraction(0) // Reset ProgressBar
-				AddText(textview, fmt.Sprintf("annie-gtk is now downloading %s => %s", URL, DestFolder))
-				// TODO: Download progress
-				output := &outputBuffer{
-					reader:   nil,
-					textview: textview,
-				}
-				savedStdout := os.Stdout
-				r, w, _ := os.Pipe()
-				output.attachReader(r, textview)
-				os.Stdout = w
-
-				defaultDownloader, data, err := setupDownloader(nil, URL)
-				if err != nil {
-					AddText(textview, "annie-backend got error while setting up downloader: "+err.Error())
-				}
-				err, Site, Title, Type, Size, FileNameLength, stream := GetInfo(defaultDownloader, data)
-				if err != nil {
-					AddText(textview, "annie-backend got error: "+err.Error())
-				}
-
-				go func() {
-					var savedSize int64
-					for {
-						if len(stream.Parts) == 1 {
-							savedSize, err = GetSize(defaultDownloader, data, Title, FileNameLength, stream.Parts[0])
+			go func() {
+				var savedSize int64
+				for {
+					if len(stream.Parts) == 1 {
+						savedSize, err = GetSize(defaultDownloader, data, Title, FileNameLength, stream.Parts[0])
+						if err != nil {
+							AddText(textview, "GetSize error:"+err.Error())
+						}
+					} else {
+						savedSize = 0
+						for index, part := range stream.Parts {
+							partFileName := fmt.Sprintf("%s[%d]", Title, index)
+							partSize, err := GetSize(defaultDownloader, data, partFileName, FileNameLength, part)
 							if err != nil {
-								AddText(textview, "GetSize error:"+err.Error())
+								AddText(textview, "GetSize error (multi parts):"+err.Error())
 							}
-						} else {
-							savedSize = 0
-							for index, part := range stream.Parts {
-								partFileName := fmt.Sprintf("%s[%d]", Title, index)
-								partSize, err := GetSize(defaultDownloader, data, partFileName, FileNameLength, part)
-								if err != nil {
-									AddText(textview, "GetSize error (multi parts):"+err.Error())
-								}
-								// AddText(textview, fmt.Sprintf("partSize[%d]: %.2f MiB", index, float64(partSize)/(1024*1024)))
-								savedSize += partSize
-							}
+							// AddText(textview, fmt.Sprintf("partSize[%d]: %.2f MiB", index, float64(partSize)/(1024*1024)))
+							savedSize += partSize
 						}
+					}
 
-						if savedSize < Size {
-							// AddText(textview, fmt.Sprintf("Downloaded %.2f MiB/%.2f MiB", float64(savedSize)/(1024*1024), float64(Size)/(1024*1024)))
-							progbar.SetFraction(float64(savedSize) / float64(Size))
-							progbar.SetText(fmt.Sprintf("Downloaded %.2f MiB/%.2f MiB", float64(savedSize)/(1024*1024), float64(Size)/(1024*1024)))
-							time.Sleep(500 * time.Millisecond)
+					if savedSize < Size {
+						// AddText(textview, fmt.Sprintf("Downloaded %.2f MiB/%.2f MiB", float64(savedSize)/(1024*1024), float64(Size)/(1024*1024)))
+						progbar.SetFraction(float64(savedSize) / float64(Size))
+						progbar.SetText(fmt.Sprintf("Downloaded %.2f MiB/%.2f MiB", float64(savedSize)/(1024*1024), float64(Size)/(1024*1024)))
+						time.Sleep(500 * time.Millisecond)
+					}
+					line, err := output.reader.ReadString('\n')
+					if err == nil || err == io.EOF {
+						if line != "" {
+							// AddText(textview, "DEBUG: "+line)
 						}
-						line, err := output.reader.ReadString('\n')
-						if err == nil || err == io.EOF {
-							if line != "" {
-								// AddText(textview, "DEBUG: "+line)
-							}
-							if err == io.EOF {
-								break
-							}
-						} else {
-							AddText(textview, err.Error())
+						if err == io.EOF {
 							break
 						}
-						// fmt.Fprint(savedStdout, line)
+					} else {
+						AddText(textview, err.Error())
+						break
 					}
-					AddText(textview, "Download completed")
-					progbar.SetText("Download completed")
-					progbar.SetFraction(1)
-				}()
+					// fmt.Fprint(savedStdout, line)
+				}
+				AddText(textview, "Download completed")
+				progbar.SetText("Download completed")
+				progbar.SetFraction(1)
+			}()
 
-				go func() {
-					AddText(textview, "Downloading from: "+Site)
-					AddText(textview, "File title: "+Title)
-					AddText(textview, "File type: "+Type)
-					AddText(textview, "File size: "+fmt.Sprintf("%.2f MiB (%d Bytes)\n", float64(Size)/(1024*1024), Size))
-					if err := Download(defaultDownloader, data); err != nil {
-						AddText(textview, "Got error while downloading: "+err.Error())
-					}
+			go func() {
+				AddText(textview, "Downloading from: "+Site)
+				AddText(textview, "File title: "+Title)
+				AddText(textview, "File type: "+Type)
+				AddText(textview, "File size: "+fmt.Sprintf("%.2f MiB (%d Bytes)\n", float64(Size)/(1024*1024), Size))
+				if err := Download(defaultDownloader, data); err != nil {
+					AddText(textview, "Got error while downloading: "+err.Error())
+				}
 
-					err := w.Close()
-					if err != nil {
-						AddText(textview, "Got error while closing the output: "+err.Error())
-					}
-					os.Stdout = savedStdout
-				}()
-			} else {
-				AddText(textview, "You typed something. but not valid URL!")
-			}
-		})
+				err := w.Close()
+				if err != nil {
+					AddText(textview, "Got error while closing the output: "+err.Error())
+				}
+				os.Stdout = savedStdout
+			}()
+		} else {
+			AddText(textview, "You typed something. but not valid URL!")
+		}
+	})
 
-		menuitem1.Connect("select", func(menuitem1 *gtk.MenuItem) {
-			about := About(exPath)
-			about.SetTransientFor(win)
-			about.Show()
-		})
-
-		// Show the Window and all of its components.
-		win.Show()
-		application.AddWindow(win)
+	menuitem1.Connect("select", func(menuitem1 *gtk.MenuItem) {
+		about := About(exPath)
+		about.SetTransientFor(win)
+		about.Show()
 	})
 
 	// Launch the application
-	os.Exit(application.Run(os.Args))
+	win.ShowAll()
 }
 
 // Modified from fanaticscripter/annie-mingui
@@ -199,12 +213,56 @@ func (b *outputBuffer) attachReader(r io.Reader, textview *gtk.TextView) {
 	b.textview = textview
 }
 
-func isWindow(obj glib.IObject) (*gtk.Window, error) {
-	// Make type assertion (as per gtk.go).
-	if win, ok := obj.(*gtk.Window); ok {
-		return win, nil
+func setupWindow(title string, application *gtk.Application) *gtk.ApplicationWindow {
+	win, err := gtk.ApplicationWindowNew(application)
+	if err != nil {
+		log.Fatal("Unable to create window:", err)
 	}
-	return nil, errors.New("not a *gtk.Window")
+
+	win.SetTitle(title)
+	win.SetPosition(gtk.WIN_POS_CENTER)
+	win.SetDefaultSize(800, 600)
+	return win
+}
+
+func setupBox(orient gtk.Orientation) *gtk.Box {
+	box, err := gtk.BoxNew(orient, 2)
+	if err != nil {
+		log.Fatal("Unable to create box:", err)
+	}
+	return box
+}
+
+func setupLabel(text string) *gtk.Label {
+	label, err := gtk.LabelNew(text)
+	if err != nil {
+		log.Fatal("Unable to create label:", err)
+	}
+	return label
+}
+
+func setupMenuBar() *gtk.MenuBar {
+	menubar, err := gtk.MenuBarNew()
+	if err != nil {
+		log.Fatal("Unable to create MenuBar:", err)
+	}
+	return menubar
+}
+
+func setupMenuItem() *gtk.MenuItem {
+	menuitem, err := gtk.MenuItemNewWithLabel("关于")
+	if err != nil {
+		log.Fatal("Unable to create MenuItem:", err)
+	}
+	return menuitem
+}
+
+func setupTextView() *gtk.TextView {
+	tv, err := gtk.TextViewNew()
+	if err != nil {
+		log.Fatal("Unable to create TextView:", err)
+	}
+	return tv
 }
 
 func GetBuffer(tv *gtk.TextView) *gtk.TextBuffer {
